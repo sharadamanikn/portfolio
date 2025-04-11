a7d4850990afe74cac19e220bda7364cffb4b915
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-11 15:41:59 +0530

    Add About component with personal information and career aspirations

---
+++ src/components/About/About.tsx
@@ -0,0 +1,31 @@
+import "./About.css";
+const About = (): React.ReactNode => {
+	return (
+<section id="about" className="about-section">
+<div className="about-container">
+				<div className="about-label">
+					<h2>About</h2>
+				</div>
+				<div className="about-content">
+					<p>
+						I'm Sharadamani K N. I am working as an intern at
+						Stacklane.
+					</p>
+					<p>
+						Currently pursuing my final year B.E in Information Science and
+						Engineering at Siddaganga Institute of Technology, Tumkur.
+					</p>
+					<p>
+						With a degree in Computer Science, I aspire to pursue my M.Tech to
+						deepen my knowledge in the field of development.
+					</p>
+					<p>
+						I find this field of Computer Science to be fasinating to reach greater heights in my career.
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default About;
